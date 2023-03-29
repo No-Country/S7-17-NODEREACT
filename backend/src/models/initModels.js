072bfaf13e5a5a6db6db181d8048acabc857e84c
Franco <francocorniglione5@gmail.com>
2023-03-29 19:00:40 -0300

    achievement-user and advantage-user relations

--- backend/src/models/initModels.js
+++ backend/src/models/initModels.js
@@ -1,5 +1,15 @@
 const { Achievement, Advantage } = require("./index");
 
-const initModels = () => {};
+const initModels = () => {
+  /* ----------------------- Relación Achievement - User ----------------------- */
+
+  Achievement.belongsToMany(User, { through: "user_achievement" });
+  User.belongsToMany(Achievement, { through: "user_achievement" });
+
+  /* ----------------------- Relación Advantage   - User ----------------------- */
+
+  Advantage.belongsToMany(User, { through: "user_advantage" });
+  User.belongsToMany(Advantage, { through: "user_advantage" });
+};
 
 module.exports = initModels;
