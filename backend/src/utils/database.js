d6e981097b20e02cd3c8d01347bdd6478be8b810
Franco <francocorniglione5@gmail.com>
2023-03-29 12:56:03 -0300

    changing console.log messages

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -2,11 +2,11 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize({
-  database: process.env.DB_NAME,
   username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   dialect: "postgres",
   logging: false
 });
