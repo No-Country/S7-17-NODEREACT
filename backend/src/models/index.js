ea7a4667551bc4830a56d23c5b1e4019c663e500
Franco <francocorniglione5@gmail.com>
2023-03-31 19:33:55 -0300

    models stuff

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,6 +1,6 @@
-const Users = require("./users.model");
-const RoomMatch = require("./roomMatch.model");
-const UsersFriends = require("./usersFrineds.model");
+const User = require("./User.model");
+const User_Friend = require("./User_Friend.model");
+const Room_Match = require("./Room_Match.model");
 const Achievement = require("./Achievement.model");
 const Advantage = require("./Advantage.model");
 const News = require("./News.model");
@@ -12,9 +12,9 @@ const User_Advantage = require("./User_Advantages.model");
 const User_Topic = require("./User_Topic.model");
 
 module.exports = {
-  Users,
-  RoomMatch,
-  UsersFriends,
+  User,
+  User_Friend,
+  Room_Match,
   Achievement,
   Advantage,
   News,
