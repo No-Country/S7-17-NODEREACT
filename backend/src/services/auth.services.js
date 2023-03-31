5e0eba85c1e638c611a87a30171dab344e6492fa
Franco <francocorniglione5@gmail.com>
2023-03-31 19:27:15 -0300

    services stuff

--- backend/src/services/auth.services.js
+++ backend/src/services/auth.services.js
@@ -1,4 +1,4 @@
-const { Users } = require("../models");
+const { User } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -7,10 +7,10 @@ class AuthServices {
   static async authenticate(credentials) {
     try {
       const { username, password, socketId } = credentials;
-      const getUser = await Users.findOne({
+      const getUser = await User.findOne({
         where: { username }
       });
-      await Users.update({ socketId }, { where: { id: getUser.id } });
+      await User.update({ socketId }, { where: { id: getUser.id } });
       console.log(credentials);
       if (!getUser) throw "User not found.";
       const isValid = bcrypt.compareSync(password, getUser.password);
@@ -20,7 +20,7 @@ class AuthServices {
       throw error;
     }
   }
-  static genToken(data) {
+  static generateToken(data) {
     try {
       const token = jwt.sign(data, process.env.SECRET_KEY, {
         algorithm: "HS512"
