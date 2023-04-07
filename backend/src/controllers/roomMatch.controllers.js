c73ab29a8aaa9f0ef4757a761eef49a9d1439daf
Luis Uzcategui <alfonsouzcategui2@gmail.com>
2023-04-07 14:59:01 -0500

    users friends

--- backend/src/controllers/roomMatch.controllers.js
+++ backend/src/controllers/roomMatch.controllers.js
@@ -15,41 +15,6 @@ const createRoomSolitary = async (req, res, next) => {
   }
 };
 
-<<<<<<< HEAD
-=======
-const createRoomFriend = async (req, res, next) => {
-  try {
-    const body = req.body;
-
-    const result = await RoomServices.createRoomFriend(body);
-    res.status(201).json(result);
-  } catch (error) {
-    next({
-      status: 400,
-      message: "Error al crear Room (friends)",
-      errorContent: error
-    });
-  }
-};
-
-const createRoomRandom = async (req, res, next) => {
-  try {
-    const body = req.body;
-
-    const result = await RoomServices.createRoomRandom(body);
-    if (!result) return res.status(404).json({ message: "Opponent not found" });
-
-    return res.status(201).json(result);
-  } catch (error) {
-    next({
-      status: 400,
-      message: "Error al crear Room (random)",
-      errorContent: error
-    });
-  }
-};
-
->>>>>>> origin/develop
 const getRoomById = async (req, res, next) => {
   try {
     const id = req.params.id;
