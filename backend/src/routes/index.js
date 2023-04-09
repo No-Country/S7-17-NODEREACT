61e2759a3b31a26893cc7db029e01813c2cee2ba
Franco <francocorniglione5@gmail.com>
2023-04-09 17:32:41 -0300

    routes stuff

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,5 +2,6 @@ const UserRoutes = require("./user.routes");
 const AuthRoutes = require("./auth.routes");
 const UserFriendRoutes = require("./userFriend.routes");
 const RoomMatchRoutes = require("./roomMatch.routes");
+const AchievementRoutes = require("./achievement.routes");
 
-module.exports = { UserRoutes, AuthRoutes, UserFriendRoutes, RoomMatchRoutes };
+module.exports = { UserRoutes, AuthRoutes, UserFriendRoutes, RoomMatchRoutes, AchievementRoutes };
