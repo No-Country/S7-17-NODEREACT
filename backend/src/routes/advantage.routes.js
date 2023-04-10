968cd9f82eaa9b1dc633d4e846bebceb4720d7de
Franco <francocorniglione5@gmail.com>
2023-04-10 11:25:45 -0300

    routes stuff

---
+++ backend/src/routes/advantage.routes.js
@@ -0,0 +1,60 @@
+const { Router } = require("express");
+const { getAdvantages, getUserAdvantages } = require("../controllers");
+
+const router = Router();
+
+/**
+ * @openapi
+ * /api/v1/advantages/all:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get an array of all advantages.
+ *     tags: [Advantages]
+ *     responses:
+ *       200:
+ *         description: The advantages were successfully found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Advantage'
+ * /api/v1//advantages/user/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get an array of advantages from a particular user by their id.
+ *     tags: [Advantages]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The User id
+ *     responses:
+ *       200:
+ *         description: The advantages of the user were successfully found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   quantity:
+ *                     type: integer
+ *                   advantageId:
+ *                     type: integer
+ *               example:
+ *                 - quantity: 10
+ *                   advantageId: 1
+ *                 - quantity: 5
+ *                   advantageId: 2
+ */
+
+router.get("/advantages/all", getAdvantages);
+router.get("/advantages/user/:id", getUserAdvantages);
+
+module.exports = router;
