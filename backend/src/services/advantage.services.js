c15ab15d65761ecccd4a38c1f62b3188d8687dc9
Franco <francocorniglione5@gmail.com>
2023-04-10 13:12:36 -0300

    adding update service

--- backend/src/services/advantage.services.js
+++ backend/src/services/advantage.services.js
@@ -1,4 +1,5 @@
 const { Advantage, User_Advantage } = require("../models");
+const sequelize = require("sequelize");
 
 class AdvantageServices {
   static async getAdvantages() {
@@ -19,6 +20,25 @@ class AdvantageServices {
       });
       const remainingAdvantages = userAdvantages.map(advantage => advantage.dataValues);
       return remainingAdvantages;
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async updateUserAdvantages(id, body) {
+    try {
+      const { buy, advantageId, quantity } = body;
+
+      if (buy) {
+        User_Advantage.update(
+          { quantity: sequelize.literal(`quantity + ${quantity}`) },
+          { where: { userId: id, advantageId } }
+        );
+      } else {
+        User_Advantage.update(
+          { quantity: sequelize.literal("quantity - 1") },
+          { where: { userId: id, advantageId } }
+        );
+      }
     } catch (error) {
       throw error;
     }
