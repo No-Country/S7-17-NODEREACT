8d0c26c244afd3dc6b95c5b2de7c7dff93e53f4c
Franco <francocorniglione5@gmail.com>
2023-04-10 11:27:38 -0300

    swagger doc stuff

--- backend/src/models/Advantage.model.js
+++ backend/src/models/Advantage.model.js
@@ -1,6 +1,36 @@
 const db = require("../utils/database");
 const { DataTypes } = require("sequelize");
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Advantage:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The id of the Advantage.
+ *         name:
+ *           type: string
+ *           description: The name of the Advantage.
+ *         description:
+ *           type: string
+ *           description: The description of the Advantage.
+ *         iconImg:
+ *           type: string
+ *           description: The image's url of the Advantage.
+ *         price:
+ *           type: integer
+ *           description: The price of the Advantage.
+ *       example:
+ *         id: 1
+ *         name: 'Martillo'
+ *         description: 'Elimina 2 opciones de las 4 posibles.'
+ *         iconImg: 'https://mi.imagen.com/icono-martillo.jpg'
+ *         price: 20
+ */
+
 const Advantage = db.define(
   "advantage",
   {
