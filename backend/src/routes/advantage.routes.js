7e420122b2c6d8cf88ae49c1f15103d8f70d9ea8
Franco <francocorniglione5@gmail.com>
2023-04-11 17:21:12 -0300

    deleting routes

--- backend/src/routes/advantage.routes.js
+++ backend/src/routes/advantage.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getAdvantages, getUserAdvantages, buyUserAdvantages } = require("../controllers");
+const { getAdvantages, buyUserAdvantages } = require("../controllers");
 const { authenticate } = require("../middlewares/auth.middleware");
 
 const router = Router();
@@ -21,38 +21,6 @@ const router = Router();
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/Advantage'
- * /api/v1/advantages/user/{id}:
- *   get:
- *     security:
- *       - bearerAuth: []
- *     summary: Get an array of advantages from a particular user by their id.
- *     tags: [Advantages]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *         required: true
- *         description: The User id
- *     responses:
- *       200:
- *         description: The advantages of the user were successfully found.
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   quantity:
- *                     type: integer
- *                   advantageId:
- *                     type: integer
- *               example:
- *                 - quantity: 10
- *                   advantageId: 1
- *                 - quantity: 5
- *                   advantageId: 2
  * /api/v1/advantages/update/user/{id}:
  *   put:
  *     security:
@@ -86,7 +54,6 @@ const router = Router();
  */
 
 router.get("/advantages/all", authenticate, getAdvantages);
-router.get("/advantages/user/:id", authenticate, getUserAdvantages);
 router.put("/advantages/update/user/:id", authenticate, buyUserAdvantages);
 
 module.exports = router;
