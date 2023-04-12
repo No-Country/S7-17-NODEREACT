df877438524915c44e3f1a3a9b91cb14da7ddf14
Franco <francocorniglione5@gmail.com>
2023-04-12 20:20:17 -0300

    models stuff

--- backend/src/models/News.model.js
+++ backend/src/models/News.model.js
@@ -1,6 +1,35 @@
 const db = require("../utils/database");
 const { DataTypes } = require("sequelize");
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     News:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The id of the News.
+ *         title:
+ *           type: string
+ *           description: The name of the News.
+ *         summary:
+ *           type: string
+ *           description: The description of the News.
+ *         img:
+ *           type: string
+ *           description: The image's url of the News.
+ *         link:
+ *           type: string
+ *           description: The price of the News.
+ *       example:
+ *         title: 'Gran descubrimiento espacial'
+ *         summary: 'Un impresionante hallazgo espacial se ha dado a conocer en las últimas horas. Si quieres conocer más, entra al link.'
+ *         img: 'https://galeriadeimagenes.com/via-lactea.jpg'
+ *         link: 'https://eldiario.com/noticias/gran-descubrimiento-espacial'
+ */
+
 const News = db.define(
   "news",
   {
