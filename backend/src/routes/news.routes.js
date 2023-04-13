c1ee2532ab1bc937d0b759c0ee1ee98be88ad2c6
Franco <francocorniglione5@gmail.com>
2023-04-13 13:01:37 -0300

    routes stuff

--- backend/src/routes/news.routes.js
+++ backend/src/routes/news.routes.js
@@ -1,11 +1,26 @@
 const { Router } = require("express");
-const { getNewsByUserTopics } = require("../controllers");
+const { getNews, getNewsByUserTopics } = require("../controllers");
 const { authenticate } = require("../middlewares/auth.middleware");
 
 const router = Router();
 
 /**
  * @openapi
+ * /api/v1/news/all:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get an array of all news.
+ *     tags: [News]
+ *     responses:
+ *       200:
+ *         description: The news were successfully found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/News'
  * /api/v1/news/topic/user/{id}:
  *   get:
  *     security:
@@ -27,9 +42,29 @@ const router = Router();
  *             schema:
  *               type: array
  *               items:
- *                 $ref: '#/components/schemas/News'
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                     example: 'Gran descubrimiento espacial'
+ *                   summary:
+ *                     type: string
+ *                     example: 'Un impresionante hallazgo espacial se ha dado a conocer en las últimas horas. Si quieres conocer más, entra al link.'
+ *                   img:
+ *                     type: string
+ *                     example: 'https://galeriadeimagenes.com/via-lactea.jpg'
+ *                   link:
+ *                     type: string
+ *                     example: 'https://eldiario.com/noticias/gran-descubrimiento-espacial'
+ *                   topic:
+ *                     type: object
+ *                     properties:
+ *                       name:
+ *                         type: string
+ *                         example: 'Ciencias y Naturaleza'
  */
 
-router.get("/news/topic/user/:id", getNewsByUserTopics);
+router.get("/news/all", authenticate, getNews);
+router.get("/news/topic/user/:id", authenticate, getNewsByUserTopics);
 
 module.exports = router;
