125ba82c34a1bf8bb29a280cc5c8e1d3721e2930
Franco <francocorniglione5@gmail.com>
2023-04-13 11:25:00 -0300

    services stuff

--- backend/src/services/news.services.js
+++ backend/src/services/news.services.js
@@ -1,14 +1,35 @@
-const { News } = require("../models");
+const { News, Topic, User_Topic, User } = require("../models");
+const { Op } = require("sequelize");
 
 class NewsServices {
-  static async getNewsByTopic(id) {
+  static async getNewsByUserTopics(id) {
     try {
-      if (!id) throw "Id not found";
+      if (!id) throw "Ids not found";
       if (isNaN(parseInt(id))) throw "Id param must be an integer";
 
+      // const { topics } = await User.findByPk(id, {
+      //   attributes: [],
+      //   include: {
+      //     model: Topic,
+      //     as: "topics",
+      //     attributes: ["id"],
+      //     through: {
+      //       attributes: []
+      //     }
+      //   }
+      // });
+
+      const topics = await User_Topic.findAll({ where: { userId: id }, attributes: ["topicId"] });
+      const topicId = topics.map(topic => topic.dataValues.topicId);
+
       const news = await News.findAll({
-        where: { topicId: id },
-        attributes: ["title", "summary", "img", "link"]
+        where: {
+          topicId: {
+            [Op.and]: [topicId]
+          }
+        },
+        attributes: ["title", "summary", "img", "link"],
+        include: { model: Topic, attributes: ["name"] }
       });
       return news;
     } catch (error) {
