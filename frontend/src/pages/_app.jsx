4ead88963ff256922d0a632bf16aa489ddf73596
Franco <francocorniglione5@gmail.com>
2023-04-16 17:05:18 -0300

    adding some toast messages

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -3,6 +3,8 @@ import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   let persistor = persistStore(store);
@@ -10,6 +12,7 @@ export default function App({ Component, pageProps }) {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Component {...pageProps} />
+        <ToastContainer />
       </PersistGate>
     </Provider>
   );
