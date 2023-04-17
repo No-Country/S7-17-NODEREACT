5165982fdb5b53b57bb2e1c15e62ee44a87e92fd
Franco <francocorniglione5@gmail.com>
2023-04-17 13:44:19 -0300

    creating new reducer

---
+++ frontend/src/features/socket/socketSlice.js
@@ -0,0 +1,18 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+export const socketSlice = createSlice({
+  name: "socket",
+  initialState: {},
+  reducers: {
+    initSocket: (state, action) => {
+      return action.payload;
+    }
+  }
+});
+
+export const setSocket = socket => dispatch => {
+  dispatch(initSocket(socket));
+};
+
+export const { initSocket } = socketSlice.actions;
+export default socketSlice.reducer;
