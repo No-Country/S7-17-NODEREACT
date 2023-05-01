b0f619dd1c7a45d9e1188a6bd8a778070ca5d5b7
Franco <francocorniglione5@gmail.com>
2023-05-01 17:03:47 -0300

    pages stuff

--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -1,55 +1,30 @@
 import Layout from "@/components/layout";
 import styles from "../profile/styles.module.css";
-import { useSelector } from "react-redux";
 import editIcon from "../../assets/edit-icon.svg";
 import coinIcon from "../../assets/coin-icon.svg";
 import hammerIcon from "../../assets/hammer-icon.svg";
 import wandIcon from "../../assets/magic-wand-icon.svg";
 import Image from "next/image";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+import { clearLoggedUser } from "@/features/auth/authSlice";
+import { toast } from "react-toastify";
+import { toastProperties } from "@/styles/toastProperties";
 
 const Profile = () => {
-  const dataLogin = useSelector(state => state.auth);
-  const [perfil, setPerfil] = useState({
-    id: null,
-    username: "",
-    email: "",
-    lifes: null,
-    points: null,
-    coins: null,
-    profileImg: "",
-    advantages: [
-      {
-        user_advantage: {
-          quantity: null,
-          advantageId: null
-        }
-      },
-      {
-        user_advantage: {
-          quantity: null,
-          advantageId: null
-        }
-      }
-    ]
-  });
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const currentUser = localStorage.getItem("user") || {};
+  const user = JSON.parse(currentUser);
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}/user/${dataLogin.id}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${dataLogin.token}`
-        }
-      })
-      .then(res => {
-        localStorage.setItem("user", JSON.stringify(res.data));
-        setPerfil(res.data);
-      })
-      .catch(err => console.log(err));
-  }, []);
+  const clearCurrentUser = () => {
+    localStorage.removeItem("user");
+    dispatch(clearLoggedUser());
+    toast.success("¡Hasta la vista! Esperamos verte pronto.", toastProperties);
+    setTimeout(() => {
+      router.push("/");
+    }, 4000);
+  };
 
   return (
     <Layout>
@@ -62,7 +37,7 @@ const Profile = () => {
             </div>
             <div className={styles.img__container}>
               <div className={styles.imgrounded}>
-                <img className={styles.img} src={perfil.profileImg} alt="" />
+                <img className={styles.img} src={user?.profileImg} alt="" />
                 <div className={styles.edit__img}>
                   <Image width={22} height={22} src={editIcon} alt="" />
                 </div>
@@ -70,7 +45,7 @@ const Profile = () => {
             </div>
             <div className={styles.username__container}>
               <div className={styles.username}>
-                <p>{perfil.username}</p>
+                <p>{user?.username}</p>
                 <div className={styles.edit__user}>
                   <Image width={22} height={22} src={editIcon} alt="" />
                 </div>
@@ -78,7 +53,7 @@ const Profile = () => {
             </div>
             <div className={styles.point__container}>
               <div className={styles.point__number}>
-                <p>{perfil.points}</p>
+                <p>{user?.points}</p>
               </div>
               <div className={styles.point__title}>
                 <p>Puntos</p>
@@ -87,7 +62,7 @@ const Profile = () => {
                 <div className={styles.coin__img__container}>
                   <div className={styles.coin__img}>
                     <Image width={35} height={35} src={coinIcon} alt="" />
-                    <div className={styles.coin__number}>{perfil.coins}</div>
+                    <div className={styles.coin__number}>{user?.coins}</div>
                   </div>
                 </div>
               </div>
@@ -107,7 +82,7 @@ const Profile = () => {
                     alt=""
                   />
                   <div className={styles.hammer__number}>
-                    {perfil.advantages[0]?.user_advantage.quantity}
+                    {user?.advantages && user.advantages[0].user_advantage.quantity}
                   </div>
                 </div>
                 <p>Martillos</p>
@@ -122,7 +97,7 @@ const Profile = () => {
                     alt=""
                   />
                   <div className={styles.hammer__number}>
-                    {perfil.advantages[1]?.user_advantage.quantity}
+                    {user?.advantages && user.advantages[1].user_advantage.quantity}
                   </div>
                 </div>
                 <p>Varitas mágicas</p>
@@ -145,7 +120,7 @@ const Profile = () => {
           <div className={styles.bottom__link}>
             <p>Cambiar contraseña</p>
             <p>Noticias</p>
-            <p>Logout</p>
+            <p onClick={() => clearCurrentUser()}>Logout</p>
           </div>
         </div>
       </div>
